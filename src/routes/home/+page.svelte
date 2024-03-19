526583cc32da074e1034eb1ec67241af584068b9
Daniel <bart0044@hz.nl>
2024-03-19 17:19:51 +0100

    commit for tracking tailwind config fro extension

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Homepage</h1>
+
+<div id="navBarLeft" class="m-10">
+    <nav>
+        <p>smth</p>
+    </nav>
+</div>
+
+<div id="aboutMe" class="">
+    <p>Hey there! I'm Daniel, currently pursuing my Bachelor's in HBO-ICT. Passionate about technology with a thirst for knowledge and eager to apply my skills in real-world settings. I am thriving to create solutions to problems via lines of code. Open to networking and exploring opportunities in ICT. Let's connect!</p>
+</div>
