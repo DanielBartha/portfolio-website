ec1a2fc1cbe6019ab96892f341b7e28312986e99
Daniel <bart0044@hz.nl>
2024-03-21 17:28:36 +0100

    designed navbar and content, started scrollable projects

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,12 +1,15 @@
 <script>
-    import NavAndContent from "$lib/components/NavAndContent.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+    import Content from "$lib/components/Content.svelte";
+    import ProjectScroll from "$lib/components/ProjectScroll.svelte";
 </script>
 
-
 <section class="bg-gradient-to-r from-cyan-950 to-emerald-950">
+    <Navbar />
     <div class="flex justify-center items-center h-screen">
-        <div id="navBarLeft">
-            <NavAndContent />
+        <div class="flex">
+            <Content />
+            <ProjectScroll />
         </div>
     </div>
 </section>
