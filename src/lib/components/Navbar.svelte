4f701880f83cf62b0c0682df97373bcec7f646ed
Daniel <bart0044@hz.nl>
2024-03-25 16:53:35 +0100

    home page 90% finished, still need to fix scrollable fade, started about page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,15 +3,15 @@
 </script>
 
 
-<nav class="ml-6 lg:block border-r border-white h-[100%] fixed">
+<nav class="lg:block border-r border-white h-[100%] fixed">
     <div>
-        <img class="size-24" src="/images/logo.png" alt="logo">
+        <a href="/home"><img class="size-24" src="/images/logo.png" alt="logo"></a>
     </div>
-    <ul class="mt-16 w-max pr-8 py-40">
-        <li class="mb-14 text-slate-300 font-mono"><a href="/home">Home</a></li>
-        <li class="my-14 text-slate-300 font-mono">About</li>
-        <li class="my-14 text-slate-300 font-mono">Projects</li>
-        <li class="my-14 text-slate-300 font-mono">Skills</li>
-        <li class="my-14 text-slate-300 font-mono">Contact</li>
+    <ul class="mt-16 ml-4 w-max pr-5 py-40">
+        <li class="mb-14 text-slate-300 hover:text-white font-mono"><a href="/home">Home</a></li>
+        <li class="my-14 text-slate-300 hover:text-white font-mono"><a href="/about">About</a></li>
+        <li class="my-14 text-slate-300 hover:text-white font-mono">Projects</li>
+        <li class="my-14 text-slate-300 hover:text-white font-mono">Skills</li>
+        <li class="my-14 text-slate-300 hover:text-white font-mono">Contact</li>
     </ul>
 </nav>
